836754730aa8a0cf9ee7d5a2755e85bdd2672df9
nomercyzzz <ghostnomercy007@gmail.com>
2025-02-19 15:33:13 +0300

    Первоначальный комит

---
+++ server.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
+
